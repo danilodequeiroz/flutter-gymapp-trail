b4abc59dab9ce017bd43b5159ab635db010d76c2
danilodequeiroz <danilo.aq@gmail.com>
2023-07-18 01:40:28 -0300

    widget spacing improvements; widget color improvements; removes extensive list;

--- lib/ui/ui_screen_gym_exercise.dart
+++ lib/ui/ui_screen_gym_exercise.dart
@@ -15,138 +15,86 @@ class ScreenGymExercise extends StatelessWidget {
   final List<Feeling> feelings = [
     Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
     Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
-    Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
-    Feeling(id: "FE003", feeling: "Já senti alguma ativação", date: "2023-07-14")
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.blue,
         appBar: AppBar(
-          title:
-              Text("${exercise.name} / ${exercise.training}"),
+          title: Text("${exercise.name} / ${exercise.training}"),
         ),
         floatingActionButton: FloatingActionButton(
             onPressed: () {
               debugPrint("Clicked");
             },
             child: const Icon(Icons.plus_one)),
-        body: Padding(
+        body: Container(
+          margin: const EdgeInsets.all(8),
           padding: const EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+          ),
           child: ListView(
-
             children: [
-              ElevatedButton(onPressed: () {}, child: Text(AppLocalizations.of(context).sendPictureKeyLabel)),
-              Text(AppLocalizations.of(context).howToKeyLabel,
+              SizedBox(
+                  height: 250,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                              AppLocalizations.of(context).putPictureKeyLabel)),
+                      ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                              AppLocalizations.of(context).deletePictureKeyLabel))
+                    ],
+                  )),
+              const SizedBox(height: 8),
+              Text(AppLocalizations.of(context).howToDoKeyLabel,
                   style: const TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 18,
                   )),
+              const SizedBox(height: 8),
               Text(exercise.howTo),
-              const Divider(),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Divider(color: Colors.black45, thickness: 0.75),
+              ),
+              Text(AppLocalizations.of(context).howAmIFeelingKeyLabel,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18
+                  )),
+              const SizedBox(height: 8),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: List.generate(feelings.length, (index){
+                children: List.generate(feelings.length, (index) {
                   Feeling currentFeeling = feelings[index];
-                  return Text(currentFeeling.feeling);
+                  return ListTile(
+                    dense: true,
+                    contentPadding: EdgeInsets.zero,
+                    title: Text(currentFeeling.feeling),
+                    subtitle: Text(currentFeeling.date),
+                    leading: const Icon(Icons.keyboard_double_arrow_right_rounded),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete_outline),
+                      color: Colors.redAccent,
+                      onPressed: () {
+                        debugPrint("Deleted\nid:${currentFeeling.id} feeling:${currentFeeling.feeling}");
+                      }
+                    ),
+                  );
                 }),
               ),
-              const Divider(),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Divider(color: Colors.black45, thickness: 0.75)
+              ),
             ],
           ),
         ));
