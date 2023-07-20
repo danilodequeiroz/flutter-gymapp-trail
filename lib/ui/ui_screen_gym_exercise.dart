03fa93a6fe12e89719b73a9ba7d6ee2f6a4b3593
danilodequeiroz <danilo.aq@gmail.com>
2023-07-19 23:50:33 -0300

    Customize toolbat; Adding dirty method to populate end-view on ListView;

--- lib/ui/ui_screen_gym_exercise.dart
+++ lib/ui/ui_screen_gym_exercise.dart
@@ -15,6 +15,10 @@ class ScreenGymExercise extends StatelessWidget {
   final List<Feeling> feelings = [
     Feeling(id: "FE001", feeling: "Nenhuma ativação hoje", date: "2023-07-12"),
     Feeling(id: "FE002", feeling: "Pouca ativação hoje", date: "2023-07-13"),
+    Feeling(id: "FE003", feeling: "Pouca ativação hoje", date: "2023-07-14"),
+    Feeling(id: "FE004", feeling: "Alguma ativação hoje", date: "2023-07-15"),
+    Feeling(id: "FE005", feeling: "Alguma ativação hoje", date: "2023-07-16"),
+    Feeling(id: "FE006", feeling: "Não consigo mexer o braço na região", date: "2023-07-17"),
   ];
 
   @override
@@ -22,7 +26,22 @@ class ScreenGymExercise extends StatelessWidget {
     return Scaffold(
         backgroundColor: Colors.blue,
         appBar: AppBar(
-          title: Text("${exercise.name} / ${exercise.training}"),
+          title: Column(
+            children: [
+              Text(
+                exercise.name,
+                style:
+                const TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+              ),
+              Text(exercise.training, style: const TextStyle(fontSize: 15))
+            ],
+          ),
+          centerTitle: true,
+          backgroundColor: const Color(0xFF0a6D92),
+          elevation: 0,
+          toolbarHeight: 72,
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(bottom: Radius.circular(32))),
         ),
         floatingActionButton: FloatingActionButton(
             onPressed: () {
@@ -46,15 +65,20 @@ class ScreenGymExercise extends StatelessWidget {
                       ElevatedButton(
                           onPressed: () {},
                           child: Text(
-                              AppLocalizations.of(context).putPictureKeyLabel)),
+                              AppLocalizations
+                                  .of(context)
+                                  .putPictureKeyLabel)),
                       ElevatedButton(
                           onPressed: () {},
-                          child: Text(
-                              AppLocalizations.of(context).deletePictureKeyLabel))
+                          child: Text(AppLocalizations
+                              .of(context)
+                              .deletePictureKeyLabel))
                     ],
                   )),
               const SizedBox(height: 8),
-              Text(AppLocalizations.of(context).howToDoKeyLabel,
+              Text(AppLocalizations
+                  .of(context)
+                  .howToDoKeyLabel,
                   style: const TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 18,
@@ -65,38 +89,60 @@ class ScreenGymExercise extends StatelessWidget {
                 padding: EdgeInsets.all(8.0),
                 child: Divider(color: Colors.black45, thickness: 0.75),
               ),
-              Text(AppLocalizations.of(context).howAmIFeelingKeyLabel,
+              Text(AppLocalizations
+                  .of(context)
+                  .howAmIFeelingKeyLabel,
                   style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18
-                  )),
+                      fontWeight: FontWeight.bold, fontSize: 18)),
               const SizedBox(height: 8),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: List.generate(feelings.length, (index) {
-                  Feeling currentFeeling = feelings[index];
-                  return ListTile(
-                    dense: true,
-                    contentPadding: EdgeInsets.zero,
-                    title: Text(currentFeeling.feeling),
-                    subtitle: Text(currentFeeling.date),
-                    leading: const Icon(Icons.keyboard_double_arrow_right_rounded),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.delete_outline),
-                      color: Colors.redAccent,
-                      onPressed: () {
-                        debugPrint("Deleted\nid:${currentFeeling.id} feeling:${currentFeeling.feeling}");
-                      }
-                    ),
-                  );
-                }),
+                children: getListChildren(),
               ),
               const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Divider(color: Colors.black45, thickness: 0.75)
-              ),
+                  padding: EdgeInsets.all(8.0),
+                  child: Divider(color: Colors.black45, thickness: 0.75)),
             ],
           ),
         ));
+  }
+
+  List<Widget> getListChildren() {
+    List<Widget> valor = List.generate(feelings.length, (index) {
+      Feeling currentFeeling = feelings[index];
+      return ListTile(
+        dense: true,
+        contentPadding: EdgeInsets.zero,
+        title: Text(currentFeeling.feeling),
+        subtitle: Text(currentFeeling.date),
+        leading: const Icon(Icons.keyboard_double_arrow_right_rounded),
+        trailing: IconButton(
+            icon: const Icon(Icons.delete_outline),
+            color: Colors.redAccent,
+            onPressed: () {
+              debugPrint(
+                  "Deleted\nid:${currentFeeling.id} feeling:${currentFeeling
+                      .feeling}");
+            }),
+      );
+    });
+    valor.add(const ListTile(
+      title: Center(
+          child:
+          Column(children: [
+            Text("Fim da Lista",
+                style: TextStyle(
+                  fontSize: 14,
+                )),
+            Text("(;",
+                style: TextStyle(
+                  fontSize: 14,
+                ))
+          ],)
+      ),
+      // leading: Icon(Icons.keyboard_double_arrow_right_rounded),
+      // trailing: Icon(Icons.keyboard_double_arrow_left_rounded),
+    ));
+    return valor;
   }
 }
