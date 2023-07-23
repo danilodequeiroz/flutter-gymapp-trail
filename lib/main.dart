080e0853eada8d76ba63d9fbfe1d9d75392afae4
danilodequeiroz <danilo.aq@gmail.com>
2023-07-23 20:54:29 -0300

    improvise widget home getter on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gymapp/ui/ui_screen_auth.dart';
 import 'package:flutter_gymapp/ui/ui_screen_gym_exercise.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:logging_to_logcat/logging_to_logcat.dart';
 import 'package:logging/logging.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-
 final navigatorKey = GlobalKey<NavigatorState>();
+const mainScreen = "auth";
 
 void main() {
   initLogging();
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(scaffoldBackgroundColor:  Colors.pink),
+      theme: ThemeData(scaffoldBackgroundColor: Colors.pink),
       navigatorKey: navigatorKey,
       localizationsDelegates: const [
         AppLocalizations.delegate, // Add this line
@@ -37,7 +38,15 @@ class MyApp extends StatelessWidget {
         Locale('en'),
         Locale('pt'),
       ],
-      home: ScreenGymExercise (),
+      home: getHomeWidget(),
     );
+  }
+
+  Widget getHomeWidget() {
+    return switch (mainScreen) {
+      'auth' => const ScreenAuth(),
+      'gym_exercise' => const ScreenGymExercise(),
+      _ => const ScreenAuth()
+    };
   }
 }
