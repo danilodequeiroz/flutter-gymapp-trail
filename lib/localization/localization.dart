2f0bfa05cd4dc683397e7ee2a756a5c395726018
danilodequeiroz <danilo.aq@gmail.com>
2023-07-25 03:41:49 -0300

    implements localized messages on validation messages;

--- lib/localization/localization.dart
+++ lib/localization/localization.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-AppLocalizations appLocalization(BuildContext context2) {
-  return AppLocalizations.of(context2);
+AppLocalizations appLocalization(BuildContext context) {
+  return AppLocalizations.of(context);
 }
